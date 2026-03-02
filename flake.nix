ba0b44e67312fe587674f2cd9ea416c243b484a4
骑士姬 <2067834160@qq.com>
2026-03-02 12:05:45 +0800

    feat: 启用 flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # 导入主配置
+       ./configuration.nix
+        # 导入其他模块
+        ./chinese.nix
+      ];
+    }; 
+  };
+}
