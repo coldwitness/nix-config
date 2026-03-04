e0a12f7514186731f85439e9de97fe217da9b981
骑士姬 <2067834160@qq.com>
2026-03-04 14:06:31 +0800

    feat: 配置 kitty

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -6,18 +6,6 @@
 }:
 {
   programs = {
-    bash = {
-      enable = true;
-      interactiveShellInit = ''
-        # 检查父进程不是 fish, 并且不是在执行脚本
-        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-        then
-          # 如果是登录 shell, 传递 --login 参数
-          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-        fi
-      '';
-    };
     fish = {
       enable = true;
     };
