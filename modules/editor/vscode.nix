deabdeb33b2e43356690e5aaa4f34bc050e5d1a1
骑士姬 <2067834160@qq.com>
2026-03-04 14:06:47 +0800

    feat: 添加 vscode 插件

--- modules/editor/vscode.nix
+++ modules/editor/vscode.nix
@@ -8,11 +8,16 @@
   programs.vscode = {
     enable = true;
     extensions = with pkgs.vscode-extensions; [
-      # nexmoe.monitor-pro
       ms-ceintl.vscode-language-pack-zh-hans
       pkief.material-icon-theme
       mhutchie.git-graph
       jnoortheen.nix-ide
+      w88975.code-translate
+      usernamehw.errorlens
+      oderwat.indent-rainbow
+      christian-kohler.path-intellisense
+      tamasfe.even-better-toml
+      shd101wyy.markdown-preview-enhanced
     ];
   };
 }
