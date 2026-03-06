48e7b297e2b62c6d58e1eb9781df8c169afc81de
骑士姬 <2067834160@qq.com>
2026-03-06 12:57:28 +0800

    feat: 添加 fzf

--- users/admin/config/fish/config.fish
+++ users/admin/config/fish/config.fish
@@ -1,4 +1,22 @@
-# 启用 starship
-starship init fish | source
 # 忽略问候
 set -g fish_greeting
+
+# 启用 fzf
+fzf --fish | source
+
+# 启用 starship
+starship init fish | source
+
+function nhm
+    set -l choice (printf "1: 重建\n2: 更新\n3: 清理" | fzf --prompt="选择操作: " --height=7 --border)
+    switch "$choice"
+        case "1: 重建"
+            nh os switch --ask
+        case "2: 更新"
+            nh os switch --ask --update
+        case "3: 清理"
+            nh clean all
+        case '*'
+            echo "操作已取消"
+    end
+end
