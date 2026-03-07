60c133115a9677fea4eff2b2ae7c3c533ddd085d
骑士姬 <2067834160@qq.com>
2026-03-07 20:16:23 +0800

    perf: 简化 vscode 插件包装

--- modules/editor/vscode/vscode-latest.nix
+++ modules/editor/vscode/vscode-latest.nix
@@ -6,35 +6,22 @@
 }:
 let
   # 包装最新版 vscode
-  newVersion = "1.110.1";
+  version = "1.110.1";
   platform = "linux-x64";
-  downloadUrl = "https://update.code.visualstudio.com/${newVersion}/${platform}/stable";
-  fakeHash = "sha256-nXxPuopZX8gOWrco++VYl0AAsCxDUykkoSgWOiHFUYw=";
+  url = "https://update.code.visualstudio.com/${version}/${platform}/stable";
+  hash = "sha256-nXxPuopZX8gOWrco++VYl0AAsCxDUykkoSgWOiHFUYw=";
   vscode-latest = pkgs.vscode.overrideAttrs (oldAttrs: {
-    version = newVersion;
+    inherit version;
     src = pkgs.fetchurl {
-      url = downloadUrl;
-      hash = fakeHash;
-      name = "VSCode_${newVersion}_${platform}.tar.gz";
+      inherit url;
+      inherit hash;
+      name = "VSCode_${version}_${platform}.tar.gz";
     };
   });
-  # 包装插件
-  knightfemale = {
-    vscode-python-envs = pkgs.callPackage ./vscode-python-envs.nix {
-      version = "1.21.10651016";
-      # 如需自动更新脚本, 可传入
-      # vscode-extension-update-script = pkgs.vscode-extension-update-script;
-    };
-    monitor-pro = pkgs.callPackage ./monitor-pro.nix {
-        version = "0.6.1";
-        hash = "sha256-aqlsNt5fiR2LKlIkZj+jUcJhQHLCk7G+WW+Z/FtgzIw=";
-    };
-  };
 in
 {
   programs.vscode = {
     enable = true;
-    # 用覆盖后的包
     package = vscode-latest;
     extensions = with pkgs.vscode-extensions; [
       ms-ceintl.vscode-language-pack-zh-hans
@@ -52,9 +39,20 @@ in
       ms-python.debugpy
       ms-python.vscode-pylance
       ms-python.black-formatter
-      knightfemale.vscode-python-envs
-      knightfemale.monitor-pro
       adpyke.codesnap
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "monitor-pro";
+        publisher = "nexmoe";
+        version = "0.6.1";
+        sha256 = "sha256-UB46ElSLqsxe3idNjFc3VDeYxzwD+Fz9AA9qUV/b2ys=";
+      }
+      {
+        name = "vscode-python-envs";
+        publisher = "ms-python";
+        version = "1.21.10651016";
+        sha256 = "sha256-yaQrZKdncWWjEuBp3SLNZ3kBIq7y2FiEr9xCQmgugso=";
+      }
     ];
   };
 }
