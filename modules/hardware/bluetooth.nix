f901ac3b56f5785c3526aed701f8f302fc3b9ace
骑士姬 <2067834160@qq.com>
2026-03-13 22:17:10 +0800

    feat: 配置蓝牙

---
+++ modules/hardware/bluetooth.nix
@@ -0,0 +1,10 @@
+{
+  ...
+}:
+{
+  hardware.bluetooth = {
+    enable = true;
+    # 是否在启动时启用默认的蓝牙控制器
+    powerOnBoot = false;
+  };
+}
--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -4,5 +4,6 @@
 {
   imports = [
       ./zram.nix
+      ./bluetooth.nix
     ];
 }
