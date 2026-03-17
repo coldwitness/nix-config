f1765a1e81167cfa5930786f32f2d1c73a9c4792
骑士姬 <2067834160@qq.com>
2026-03-17 18:20:03 +0800

    fix: 修复快照函数

--- users/admin/config/fish/config.fish
+++ users/admin/config/fish/config.fish
@@ -28,14 +28,14 @@ function nhm
 end
 
 function snapperm
-    set -l choice (printf "1: 列出快照\n2: 创建快照\n3: 删除快照" | fzf --prompt="选择操作: " --height=7 --border)
+    set -l choice (printf "1: 列出快照\n2: 创建快照\n3: 删除快照\n4: 回滚快照" | fzf --prompt="选择操作: " --height=8 --border)
     if test -z "$choice"
         echo "已取消"
         return 1
     end
     switch "$choice"
         case "1: 列出快照"
-            sudo snapper list
+            sudo snapper -c home list
         case "2: 创建快照"
             echo -n "输入快照备注: "
             read -l description
@@ -43,20 +43,29 @@ function snapperm
                 echo "备注不能为空"
                 return 1
             end
-            sudo snapper -c root create -c "$description"
             sudo snapper -c home create -c "$description"
             echo "已创建快照: $description"
         case "3: 删除快照"
             set -l all_snaps (mktemp)
-            sudo snapper list | tail -n +3 > $all_snaps
+            sudo snapper -c home list | tail -n +3 > $all_snaps
             set -l selected (cat $all_snaps | fzf --prompt="选择要删除的快照: " --height=7 --border)
             rm $all_snaps
             if test -n "$selected"
                 set -l num (echo $selected | awk '{print $1}')
                 set -l description (echo $selected | awk '{print $12}')
-                sudo snapper -c root delete $num
                 sudo snapper -c home delete $num
                 echo "已删除快照: $description"
+            end
+        case "4: 回滚快照"
+            set -l all_snaps (mktemp)
+            sudo snapper -c home list | tail -n +3 > $all_snaps
+            set -l selected (cat $all_snaps | fzf --prompt="选择要回滚的快照: " --height=7 --border)
+            rm $all_snaps
+            if test -n "$selected"
+                set -l num (echo $selected | awk '{print $1}')
+                set -l description (echo $selected | awk '{print $12}')
+                sudo snapper -c home undochange $num..0
+                echo "已回滚到快照: $description"
             end
     end
 end
