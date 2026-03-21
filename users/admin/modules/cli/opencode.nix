b9064d080b5919016debc2b1b10f7038025ea794
骑士姬 <2067834160@qq.com>
2026-03-21 20:50:23 +0800

    refactor: 调整组织结构

--- users/admin/modules/cli/opencode.nix
+++ users/admin/modules/cli/opencode.nix
@@ -1,18 +1,17 @@
 {
-  lib,
-  pkgs,
-  config,
-  configPath,
   ...
 }:
 {
-  home.packages = with pkgs; [
-    opencode
-  ];
-  home.file = {
-    ".config/opencode/opencode.jsonc" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${configPath}/opencode/opencode.jsonc";
-      force = true;
+  programs.opencode = {
+    enable = true;
+    settings = {
+      mcp = {
+        mcp-nixos = {
+          enabled = true;
+          type = "local";
+          command = [ "mcp-nixos" ];
+        };
+      };
     };
   };
 }
