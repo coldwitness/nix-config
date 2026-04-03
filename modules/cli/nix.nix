f8c127ac10f9cfb680fad4b07c67ab113ab69489
骑士姬 <2067834160@qq.com>
2026-04-03 14:33:08 +0800

    feat: 为 nix 添加 GITHUB_TOKEN

--- modules/cli/nix.nix
+++ modules/cli/nix.nix
@@ -1,22 +1,33 @@
 {
   lib,
+  inputs,
   hostOptions,
   ...
 }:
 let
   cfg = hostOptions.cli.nix or { };
   substituters = cfg.substituters or [ ];
+  GITHUB_TOKEN = inputs.secrets.GITHUB_TOKEN or "";
+  accessTokens =
+    if GITHUB_TOKEN != ""
+    then "access-tokens = github.com=${GITHUB_TOKEN}\n"
+    else "";
 in
 {
-  nix.settings = {
-    # 源配置
-    inherit substituters;
-    # 启用实验性功能
-    experimental-features = [
-      # nix 命令增强
-      "nix-command"
-      # flakes 支持
-      "flakes"
-    ];
+  nix = {
+    settings = {
+      # 源配置
+      inherit substituters;
+      # 启用实验性功能
+      experimental-features = [
+        # nix 命令增强
+        "nix-command"
+        # flakes 支持
+        "flakes"
+      ];
+    };
+    extraOptions = ""
+      + accessTokens
+    ;
   };
 }
