2dfbd2179323cf579a781cd97f0dc138af09b023
骑士姬 <2067834160@qq.com>
2026-04-08 15:55:29 +0800

    feat: 更新内核

--- outputs/hosts/x86_64-linux/FL8850UA/kernel.nix
+++ outputs/hosts/x86_64-linux/FL8850UA/kernel.nix
@@ -4,42 +4,42 @@
   ...
 }:
 {
-  boot.kernelPackages = pkgs.linuxPackagesFor (
-    pkgs.linux_zen.override {
-      argsOverride = rec {
-        version = "6.19.10";
-        modDirVersion = "${version}-lqx1";
+  boot.kernelPackages = let
+    # 定义一个函数, 用于构建自定义的内核包
+    linux_lqx_pkg = { fetchurl, buildLinux, ... } @ args:
+      #  调用 buildLinux 函数, 传入参数并覆盖部分属性
+      buildLinux (args // rec {
+        # 内核主版本号
+        version = "6.19.11";
+        # 模块目录版本
+        modDirVersion = "${version}-lqx2";
+        # 内核源代码的获取方式
         src = pkgs.fetchurl {
           url = "https://github.com/zen-kernel/zen-kernel/archive/refs/tags/v${modDirVersion}.tar.gz";
-          sha256 = "sha256-k8vKKrHbqXqP8tLsj66yxsyBvDn8JGUgNnKkCpH5SII=";
+          sha256 = "sha256-SpxqXN3EE4y9sTROztmTHSinWqtEZRCUVo3emHNLVis=";
         };
+        # 额外的内核补丁列表
+        kernelPatches = [ ];
         # 配置自定义内核选项
         structuredExtraConfig = with lib.kernel; {
-        # ========== 抢占策略 ==========
-          # 启用完全抢占
-          PREEMPT = lib.mkForce yes;
-          # 显式禁用其他抢占模型
-          PREEMPT_RT = lib.mkForce no;         # 完全实时抢占: 会牺牲吞吐量, 桌面环境不需要
-          PREEMPT_NONE = lib.mkForce no;       # 无抢占: 服务器风格, 延迟高
-          PREEMPT_LAZY = lib.mkForce no;       # 懒惰抢占: 不适合桌面激进优化
-          PREEMPT_VOLUNTARY = lib.mkForce no;  # 自愿抢占: 延迟高于完全抢占
-          # 核心调度, 为防御侧信道攻击将超线程配对调度, 会增加调度延迟
-          SCHED_CORE = lib.mkForce no;
-          # 动态抢占
-          PREEMPT_DYNAMIC = lib.mkForce no;
-          # BPF 调度类扩展
-          SCHED_CLASS_EXT = lib.mkForce no;
         # ========== 内核裁剪 ==========
           # GPS
-          GNSS = lib.mkForce no;
+          GNSS = no;
           # USB4 和 Thunderbolt 支持
-          USB4 = lib.mkForce no;
+          USB4 = no;
           # 火线
-          FIREWIRE = lib.mkForce no;
+          FIREWIRE = no;
           # 触摸屏
-          INPUT_TOUCHSCREEN = lib.mkForce no;
+          INPUT_TOUCHSCREEN = no;
         };
-      };
-    }
-  );
+        #  添加内核包的元数据, 设置分支名便于识别
+        extraMeta.branch = version;
+        # 允许通过 argsOverride 进一步覆盖参数
+      } // (args.argsOverride or { }));
+    #  使用 pkgs.callPackage 调用上面定义的函数, 自动解析并传入所需的依赖
+    linux_lqx = pkgs.callPackage linux_lqx_pkg { };
+  in
+  # 根据自定义内核生成完整的内核包集合
+  # 然后使用 recurseIntoAttrs 让该属性集在 nix-env 等命令中被正确展开
+  lib.recurseIntoAttrs (pkgs.linuxPackagesFor linux_lqx);
 }
