f6005b70ae3f81176ee6550e9218fd7bff311014
骑士姬 <2067834160@qq.com>
2026-04-15 13:31:30 +0800

    feat: 美化 fastfetch

--- modules/home/cli/fastfetch.nix
+++ modules/home/cli/fastfetch.nix
@@ -1,12 +1,30 @@
 {
   lib,
-  pkgs,
   opts,
   ...
 }:
 let
   cfg = opts.cli.fastfetch or { };
   finallyEnable = cfg.enable or false;
+  # 配置中使用的变量和函数
+  width = 64; # 第二列的宽度, 可自由调整
+  esc = builtins.fromJSON ''"\u001b"'';
+  line = lib.strings.replicate width "─";
+  forward = count: "${esc}[${builtins.toJSON count}C";
+  backward = count: "${esc}[${builtins.toJSON count}D";
+  mkTop = color: nameColor: name: {
+    type = "custom";
+    key = "{#${color}}╭───────────────┬${line}╮${backward (width + 14)}{#keys}";
+    "format" = "{#${nameColor}}${name} ";
+  };
+  mkEntry = color: name: type: {
+    inherit type;
+    key = "{#${color}}│ {icon}  ${name}│${forward width}│${backward (width + 1)}{#keys}";
+  };
+  mkFoot = color: {
+    type = "custom";
+    key = "{#${color}}╰───────────────┴${line}╯";
+  };
 in
 {
   config = lib.mkIf finallyEnable {
@@ -15,6 +33,9 @@ in
         enable = true;
         settings = {
           "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
+          logo = {
+            type = "none";
+          };
           display = {
             # 分隔符
             separator = " ";
@@ -34,178 +55,83 @@ in
               # 未用部分字符
               char.total = " ";
             };
-            key = {
-              # 键名对齐宽度
-              width = 6;
-            };
           };
           modules = [
-            {
-              type = "display";
-              key = "╭─󰍹";
-              keyColor = "green";
-            }
-            {
-              type = "brightness";
-              key = "├─󰃞";
-              keyColor = "green";
-            }
-            {
-              type = "battery";
-              key = "├─";
-              keyColor = "green";
-            }
-            {
-              type = "poweradapter";
-              key = "├─";
-              keyColor = "green";
-            }
-            {
-              type = "gamepad";
-              key = "├─";
-              keyColor = "green";
-            }
-            {
-              type = "bluetooth";
-              key = "├─";
-              keyColor = "green";
-            }
-            {
-              type = "sound";
-              key = "├─";
-              keyColor = "green";
-            }
-            {
-              type = "host";
-              key = "├─󰌢";
-              keyColor = "green";
-            }
-            {
-              type = "cpu";
-              key = "├─󰻠";
-              keyColor = "green";
-            }
-            {
-              type = "gpu";
-              key = "├─󰍛";
-              keyColor = "green";
-            }
-            {
-              type = "memory";
-              key = "├─󰑭";
-              keyColor = "green";
-            }
-            {
-              type = "swap";
-              key = "├─󰓡";
-              keyColor = "green";
-            }
-            {
-              type = "disk";
-              key = "╰─";
-              keyColor = "green";
-            }
-            "break"
-            {
-              type = "lm";
-              key = "╭─󰧨";
-              keyColor = "yellow";
-            }
-            {
-              type = "de";
-              key = "├─";
-              keyColor = "yellow";
-            }
-            {
-              type = "wm";
-              key = "├─";
-              keyColor = "yellow";
-            }
-            {
-              type = "theme";
-              key = "├─󰉼";
-              keyColor = "yellow";
-            }
-            {
-              type = "icons";
-              key = "├─󰀻";
-              keyColor = "yellow";
-            }
-            {
-              type = "wallpaper";
-              key = "├─󰸉";
-              keyColor = "yellow";
-            }
-            {
-              type = "terminal";
-              key = "├─";
-              keyColor = "yellow";
-            }
-            {
-              type = "terminalfont";
-              key = "├─";
-              keyColor = "yellow";
-            }
-            {
-              type = "shell";
-              key = "╰─";
-              keyColor = "yellow";
-            }
-            "break"
-            {
-              type = "title";
-              key = "╭─";
-              format = "{user-name}@{host-name}";
-              keyColor = "blue";
-            }
-            {
-              type = "os";
-              key = "├─{icon}";
-              keyColor = "blue";
-            }
-            {
-              type = "kernel";
-              key = "├─";
-              keyColor = "blue";
-            }
-            {
-              type = "packages";
-              key = "├─󰏖";
-              keyColor = "blue";
-            }
-            {
-              type = "uptime";
-              key = "├─󰅐";
-              keyColor = "blue";
-            }
-            {
-              type = "media";
-              key = "├─󰝚";
-              keyColor = "blue";
-            }
-            {
-              type = "localip";
-              key = "├─󰩟";
-              compact = true;
-              keyColor = "blue";
-            }
-            {
-              type = "publicip";
-              key = "├─󰩠";
-              keyColor = "blue";
-              timeout = 1000;
-            }
-            {
-              type = "wifi";
-              key = "├─";
-              format = "{ssid}";
-              keyColor = "blue";
-            }
-            {
-              type = "locale";
-              key = "╰─";
-              keyColor = "blue";
-            }
+            # 系统
+            (mkTop "blue" "bright_blue" "System")
+            (mkEntry "blue" "OS         " "os")
+            (mkEntry "blue" "Kernel     " "kernel")
+            (mkEntry "blue" "Packages   " "packages")
+            (mkEntry "blue" "Locale     " "locale")
+            (
+              mkEntry "blue" "OS Age     " "disk"
+              // {
+                condition = {
+                  "!system" = "macOS";
+                };
+                keyIcon = "";
+                folders = "/";
+                format = "{create-time:10} [{days} days]";
+              }
+            )
+            (
+              mkEntry "blue" "OS Age     " "disk"
+              // {
+                condition = {
+                  system = "macOS";
+                };
+                keyIcon = "";
+                folders = "/System/Volumes/VM";
+                format = "{create-time:10} [{days} days]";
+              }
+            )
+            (mkFoot "blue")
+            # 硬件
+            (mkTop "cyan" "bright_cyan" "Hardware")
+            (mkEntry "cyan" "Host       " "host")
+            (mkEntry "cyan" "Chassis    " "chassis")
+            (mkEntry "cyan" "CPU        " "cpu" // { showPeCoreCount = true; })
+            (mkEntry "cyan" "GPU        " "gpu")
+            (mkEntry "cyan" "RAM        " "memory")
+            (mkEntry "cyan" "Disk       " "disk")
+            (mkEntry "cyan" "Swap       " "swap")
+            (mkEntry "cyan" "Sound      " "sound")
+            (mkEntry "cyan" "Battery    " "battery")
+            (mkEntry "cyan" "Power      " "poweradapter")
+            (mkEntry "cyan" "Display    " "display")
+            (mkEntry "cyan" "Brightness " "brightness")
+            (mkEntry "cyan" "Bluetooth  " "bluetooth")
+            (mkEntry "cyan" "Gamepad    " "gamepad")
+            (mkFoot "cyan")
+            # 终端
+            (mkTop "yellow" "bright_yellow" "Terminal")
+            (mkEntry "yellow" "Shell      " "shell")
+            (mkEntry "yellow" "Terminal   " "terminal")
+            (mkEntry "yellow" "Editor     " "editor")
+            (mkEntry "yellow" "Theme      " "terminaltheme")
+            (mkEntry "yellow" "Font       " "terminalfont")
+            (mkFoot "yellow")
+            # 桌面
+            (mkTop "green" "bright_green" "Desktop")
+            (mkEntry "green" "G-Driver   " "gpu" // { format = "{driver}"; })
+            (mkEntry "green" "Login      " "lm")
+            (mkEntry "green" "Desktop    " "de")
+            (mkEntry "green" "WM         " "wm")
+            (mkEntry "green" "Theme      " "theme")
+            (mkEntry "green" "WM Theme   " "wmtheme")
+            (mkEntry "green" "Icons      " "icons")
+            (mkEntry "green" "Cursor     " "cursor")
+            (mkEntry "green" "Wallpaper  " "wallpaper")
+            (mkFoot "green")
+            # 状态
+            (mkTop "magenta" "bright_magenta" "Status")
+            (mkEntry "magenta" "Uptime     " "uptime")
+            (mkEntry "magenta" "Users      " "users" // { myselfOnly = true; })
+            (mkEntry "magenta" "Media      " "media")
+            (mkEntry "magenta" "Wifi       " "wifi" // { format = "{ssid}"; })
+            (mkEntry "magenta" "Local IP   " "localip")
+            (mkEntry "magenta" "Public IP  " "publicip")
+            (mkFoot "magenta")
           ];
         };
       };
