395e2b3116763ea9949b8d87e42ab6186309dee8
骑士姬 <2067834160@qq.com>
2026-04-17 15:20:25 +0800

    feat: 添加构建 pkgSets 函数

---
+++ functions/buildPkgSets.nix
@@ -0,0 +1,30 @@
+/*
+  功能:
+    根据目标平台解析并实例化 nixpkgs 包集合
+  输入参数:
+    system: 目标平台字符串(如 "x86_64-linux", "aarch64-darwin")
+  返回值:
+    包含多个对应平台的包集合属性集
+*/
+{
+  inputs,
+  ...
+}:
+system: with inputs; {
+  # 最新版 nixpkgs 包集合
+  pkgs = import nixpkgs {
+    inherit system;
+    # 允许使用非自由软件
+    config.allowUnfree = true;
+  };
+  # 不稳定版 nixpkgs 包集合
+  pkgs-unstable = import nixpkgs-unstable {
+    inherit system;
+    config.allowUnfree = true;
+  };
+  # 25.11 版本 nixpkgs 包集合
+  pkgs-2511 = import nixpkgs-2511 {
+    inherit system;
+    config.allowUnfree = true;
+  };
+}
