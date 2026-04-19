16ba1f08fab7423e7f5c21c486b3bad490da339c
骑士姬 <2067834160@qq.com>
2026-04-19 17:30:03 +0800

    feat: 添加 fastfetch 项目

--- modules/home/cli/fastfetch.nix
+++ modules/home/cli/fastfetch.nix
@@ -59,10 +59,15 @@ in
           modules = [
             # 系统
             (mkTop "blue" "bright_blue" "System")
+            (mkEntry "blue" "BIOS       " "bios")
+            (mkEntry "blue" "BootMgr    " "bootmgr")
+            (mkEntry "blue" "InitSystem " "initsystem")
             (mkEntry "blue" "OS         " "os")
             (mkEntry "blue" "Kernel     " "kernel")
             (mkEntry "blue" "Packages   " "packages")
             (mkEntry "blue" "Locale     " "locale")
+            (mkEntry "blue" "Local IP   " "localip")
+            (mkEntry "blue" "Public IP  " "publicip")
             (
               mkEntry "blue" "OS Age     " "disk"
               // {
@@ -90,18 +95,16 @@ in
             (mkTop "cyan" "bright_cyan" "Hardware")
             (mkEntry "cyan" "Host       " "host")
             (mkEntry "cyan" "Chassis    " "chassis")
+            (mkEntry "cyan" "Board      " "board")
+            (mkEntry "cyan" "TPM        " "tpm")
             (mkEntry "cyan" "CPU        " "cpu" // { showPeCoreCount = true; })
             (mkEntry "cyan" "GPU        " "gpu")
             (mkEntry "cyan" "RAM        " "memory")
-            (mkEntry "cyan" "Disk       " "disk")
             (mkEntry "cyan" "Swap       " "swap")
-            (mkEntry "cyan" "Sound      " "sound")
-            (mkEntry "cyan" "Battery    " "battery")
-            (mkEntry "cyan" "Power      " "poweradapter")
+            (mkEntry "cyan" "Disk       " "disk")
+            (mkEntry "cyan" "ZPool      " "zpool")
             (mkEntry "cyan" "Display    " "display")
-            (mkEntry "cyan" "Brightness " "brightness")
-            (mkEntry "cyan" "Bluetooth  " "bluetooth")
-            (mkEntry "cyan" "Gamepad    " "gamepad")
+            (mkEntry "cyan" "Camera     " "camera")
             (mkFoot "cyan")
             # 终端
             (mkTop "yellow" "bright_yellow" "Terminal")
@@ -114,6 +117,9 @@ in
             # 桌面
             (mkTop "green" "bright_green" "Desktop")
             (mkEntry "green" "G-Driver   " "gpu" // { format = "{driver}"; })
+            (mkEntry "green" "Vulkan     " "vulkan")
+            (mkEntry "green" "OpenGL     " "opengl")
+            (mkEntry "green" "OpenCL     " "opencl")
             (mkEntry "green" "Login      " "lm")
             (mkEntry "green" "Desktop    " "de")
             (mkEntry "green" "WM         " "wm")
@@ -125,12 +131,18 @@ in
             (mkFoot "green")
             # 状态
             (mkTop "magenta" "bright_magenta" "Status")
-            (mkEntry "magenta" "Uptime     " "uptime")
+            (mkEntry "magenta" "Datetime   " "datetime")
+            (mkEntry "magenta" "Weather    " "weather" // { timeout = 1000; })
             (mkEntry "magenta" "Users      " "users" // { myselfOnly = true; })
+            (mkEntry "magenta" "Uptime     " "uptime")
+            (mkEntry "magenta" "Power      " "poweradapter")
+            (mkEntry "magenta" "Battery    " "battery")
+            (mkEntry "magenta" "Wifi       " "wifi")
+            (mkEntry "magenta" "Bluetooth  " "bluetooth")
+            (mkEntry "magenta" "BT Radio   " "bluetoothradio")
+            (mkEntry "magenta" "Brightness " "brightness")
+            (mkEntry "magenta" "Sound      " "sound")
             (mkEntry "magenta" "Media      " "media")
-            (mkEntry "magenta" "Wifi       " "wifi" // { format = "{ssid}"; })
-            (mkEntry "magenta" "Local IP   " "localip")
-            (mkEntry "magenta" "Public IP  " "publicip")
             (mkFoot "magenta")
           ];
         };
