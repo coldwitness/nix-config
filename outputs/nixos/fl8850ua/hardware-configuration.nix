33522bbbc8e0c7ebf90cfe7e7fb74f3959a87d98
骑士姬 <2067834160@qq.com>
2026-04-24 20:08:34 +0800

    chore: 更新 linux 内核

--- outputs/nixos/fl8850ua/hardware-configuration.nix
+++ outputs/nixos/fl8850ua/hardware-configuration.nix
@@ -25,13 +25,13 @@ in
             args
             // rec {
               # 内核主版本号
-              version = "6.19.12";
+              version = "6.19.14";
               # 模块目录版本
               modDirVersion = "${version}-lqx1";
               # 内核源代码的获取方式
               src = pkgs.fetchurl {
                 url = "https://github.com/zen-kernel/zen-kernel/archive/refs/tags/v${modDirVersion}.tar.gz";
-                sha256 = "sha256-hAch2vry6D2cDwa5bUpaKLP8NNlGGvl1xpk9kT7Qjvk=";
+                sha256 = "sha256-nisZPsS0sULrRkyLwt1MoMOA54PzREtNZjTNu0IymPs=";
               };
               # 额外的内核补丁列表
               kernelPatches = [ ];
