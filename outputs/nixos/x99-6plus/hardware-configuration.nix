6050b4e1cf45db7d16d841e89a9d57c16c16177b
骑士姬 <2067834160@qq.com>
2026-05-02 23:29:08 +0800

    feat: 添加新主机

---
+++ outputs/nixos/x99-6plus/hardware-configuration.nix
@@ -0,0 +1,120 @@
+{
+  lib,
+  config,
+  modulesPath,
+  ...
+}:
+let
+  systemBootDevice = "/dev/disk/by-uuid/0042-CAFA";
+  systemFileDevice = "/dev/disk/by-uuid/bd189698-a0ee-4531-9308-129574482730";
+in
+{
+  # 硬件自动检测模块, 会根据当前硬件生成相应的内核模块列表, 并处理一些特定硬件的配置
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  # 启动相关配置
+  boot = {
+    # initrd 阶段加载的模块(根文件系统挂载前)
+    initrd = {
+      # 由 udev 自动探测加载的模块列表(会打包进 initrd)
+      # 这些模块用于在早期启动时识别硬件
+      availableKernelModules = [
+        # USB 3.0(xHCI)主机控制器驱动, 支持 USB 3.0 接口
+        "xhci_pci"
+        # SATA AHCI 控制器驱动, 支持 SATA 硬盘/SSD
+        "ahci"
+        # SCSI 磁盘驱动, 为 SATA/SAS/USB 存储提供通用支持
+        "sd_mod"
+        #
+        "nvme"
+      ];
+      # 不依赖自动探测强制加载的模块
+      # 适用于根文件系统所在设备的驱动或任何必须在早期就绪的模块
+      kernelModules = [ ];
+    };
+    # 第二阶段加载的模块(根文件系统挂载后)
+    # 这些模块由 systemd/udev 在系统启动后期加载非关键硬件或功能
+    kernelModules = [
+      # Intel 虚拟化支持(KVM 模块)
+      "kvm-intel"
+    ];
+    # 额外的第三方内核模块包
+    # 如需在 initrd 阶段加载其中的模块, 请加入 initrd.kernelModules
+    extraModulePackages = with config.boot.kernelPackages; [ ];
+  };
+  # 要挂载的文件系统
+  fileSystems = {
+    "/boot" = {
+      device = systemBootDevice;
+      fsType = "vfat";
+      options = [
+        "fmask=0022"
+        "dmask=0022"
+      ];
+    };
+    "/" = {
+      device = systemFileDevice;
+      fsType = "btrfs";
+      options = [
+        "subvol=@"
+        "noatime"
+        "compress=zstd"
+      ];
+    };
+    "/.snapshots" = {
+      device = systemFileDevice;
+      fsType = "btrfs";
+      options = [
+        "subvol=@snapshots"
+        "compress=zstd"
+      ];
+    };
+    "/home" = {
+      device = systemFileDevice;
+      fsType = "btrfs";
+      options = [
+        "subvol=@home"
+        "compress=zstd"
+      ];
+    };
+    "/home/.snapshots" = {
+      device = systemFileDevice;
+      fsType = "btrfs";
+      options = [
+        "subvol=@home-snapshots"
+        "compress=zstd"
+      ];
+    };
+    "/nix" = {
+      device = systemFileDevice;
+      fsType = "btrfs";
+      options = [
+        "subvol=@nix"
+        "noatime"
+        "compress=zstd"
+      ];
+    };
+    "/var/log" = {
+      device = systemFileDevice;
+      fsType = "btrfs";
+      options = [
+        "subvol=@log"
+        "noatime"
+        "compress=zstd"
+      ];
+    };
+    "/.swap" = {
+      device = systemFileDevice;
+      fsType = "btrfs";
+      options = [
+        "subvol=@swap"
+        "nodatacow"
+      ];
+    };
+  };
+  # 交换设备和交换文件
+  swapDevices = [ { device = "/.swap/swapfile"; } ];
+  # 指定 Nixpkgs 编译和打包时使用的主机平台架构
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  # 启用 Intel CPU 微码更新, 从可分发的固件中加载更新
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
---
+++ outputs/nixos/x99-6plus/opts.nix
@@ -0,0 +1,76 @@
+{
+  vars,
+  optSets,
+  hostName,
+  ...
+}:
+let
+  host = {
+    customOptSets = {
+      count = 1;
+      system = vars.systemTypes.x86_64-linux;
+      stateVersion = "26.05";
+      nixConfigPath = "/home/admin/workspace/nix-config";
+      cli = {
+        nix = {
+          substituters = [
+            "https://mirror.sjtu.edu.cn/nix-channels/store"
+          ];
+        };
+      };
+      service = {
+        openssh.enable = true;
+      };
+      hardware = {
+        zram.enable = true;
+        graphics.type = vars.gpuTypes.none;
+        networking = {
+          inherit hostName;
+          networkmanager.enable = false;
+          firewall.enable = false;
+        };
+        boot-loader = {
+          efiSysMountPoint = "/boot";
+          type = vars.bootLoaderTypes.systemd-boot;
+        };
+      };
+    };
+  };
+  users = {
+    root = {
+      base = {
+        hashedPassword = "$6$yk.jU.kxIAVwaoaj$zFEdwFofY8P88Ad7/a62sm5j3QxyXcQxKTvTpRMIYDgw6G4RDXZCQgHRyeOyZHLN10lKov55WJESL8t2Ia1US0";
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEHoElqa20vBDgApV3Ek5XEP7xjPyOS+FiVxLOSsHoIK"
+        ];
+      };
+    };
+    admin = {
+      base = {
+        isNormalUser = true;
+        description = "管理员";
+        extraGroups = [
+          "wheel"
+          "networkmanager"
+        ];
+        hashedPassword = "$6$Yq2f2308VGQlSDxb$v6tOVrxDvVJYSB40g8t/n2ZVw9pSARf5Gxe.ph2n.TvyXDPiruSi8Y9pEuPNi0regGL8AB8dQBmge/kNTZqxh1";
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEHoElqa20vBDgApV3Ek5XEP7xjPyOS+FiVxLOSsHoIK"
+        ];
+      };
+      predefinedOptSetsList = [
+        optSets.baseEnv
+        optSets.fishShell
+      ];
+      customOptSets = {
+        count = 1;
+        cli.zellij.enable = true;
+        shell.bash.enable = true;
+      };
+    };
+  };
+in
+{
+  inherit host;
+  inherit users;
+}
