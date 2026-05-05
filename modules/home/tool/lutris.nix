125dcdc7a4fb507c0ee55a82aea73c42aaf69daa
骑士姬 <2067834160@qq.com>
2026-05-05 11:02:10 +0800

    feat: 限制 patch 的范围

--- modules/home/tool/lutris.nix
+++ modules/home/tool/lutris.nix
@@ -10,11 +10,20 @@ let
   finallyEnable = cfg.enable or false && ((opts.desktop.type or "") != "");
   nur = inputs.nur.legacyPackages.${pkgs.stdenv.hostPlatform.system};
   inherit (nur.repos.forkprince) proton-dw-bin;
+  # 禁用 openldap 不稳定的测试
+  pkgs-patched = pkgs.extend (
+    final: prev: {
+      openldap = prev.openldap.overrideAttrs (_: {
+        doCheck = false;
+      });
+    }
+  );
 in
 {
   config = lib.mkIf finallyEnable {
     programs.lutris = {
       enable = true;
+      package = pkgs-patched.lutris;
       # 为 lutris 配合 umu-launcher 使用而添加的 proton 软件包列表
       protonPackages = [
         proton-dw-bin
