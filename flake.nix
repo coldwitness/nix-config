5c5591217d14bbcdcacc5ae9a7fcd3df3aee0c17
骑士姬 <2067834160@qq.com>
2026-05-11 14:35:55 +0800

    feat: 添加 nur-moraxyc 输入

--- flake.nix
+++ flake.nix
@@ -33,6 +33,10 @@
       url = "github:nix-community/NixOS-WSL/main";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nur-moraxyc = {
+      url = "github:Moraxyc/nur-packages/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     mcp-nixos = {
       url = "github:utensils/mcp-nixos/main";
       inputs.nixpkgs.follows = "nixpkgs";
