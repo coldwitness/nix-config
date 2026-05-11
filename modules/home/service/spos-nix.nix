7c205f0e1f98b2e8d134f53bcd0fb5519d8c383b
骑士姬 <2067834160@qq.com>
2026-05-11 22:22:29 +0800

    refactor: 用户级 sops-nix 使用正确权限的文件

--- modules/home/service/spos-nix.nix
+++ modules/home/service/spos-nix.nix
@@ -7,5 +7,5 @@
     inputs.sops-nix.homeManagerModules.sops
   ];
   # 将 SSH 密钥自动导入为 age 密钥
-  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  sops.age.sshKeyPaths = [ "/home/admin/.ssh/id_ed25519" ];
 }
