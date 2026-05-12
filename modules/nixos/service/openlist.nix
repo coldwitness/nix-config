7edde4932b8cbdf86232d7d85d48a974a20c0a80
骑士姬 <2067834160@qq.com>
2026-05-12 16:31:00 +0800

    fix: 修复 openlist 缺少密钥的问题

--- modules/nixos/service/openlist.nix
+++ modules/nixos/service/openlist.nix
@@ -14,9 +14,25 @@ in
     inputs.nur-moraxyc.nixosModules.alist
   ];
   config = lib.mkIf finallyEnable {
+    sops.secrets."alist/jwt_secret" = {
+      sopsFile = ../../../secrets/alist.yaml;
+      format = "yaml";
+      owner = "alist";
+      group = "alist";
+      mode = "0400";
+    };
+    sops.templates."jwt_secret" = {
+      content = ''
+        ${config.sops.placeholder."alist/jwt_secret"}
+      '';
+      owner = "alist";
+      group = "alist";
+      mode = "0400";
+    };
     services.alist = {
       enable = true;
       package = pkgs.openlist;
+      settings.jwt_secret._secret = config.sops.templates."alist-jwt".path;
     };
   };
 }
